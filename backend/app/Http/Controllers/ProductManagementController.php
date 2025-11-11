d83014e4e7405b802a6917baa80bcf05daeed594
patriciaayrah <ayrah0421.pao@gmail.com>
2025-11-12 02:48:28 +0800

    Log inventory activities

--- backend/app/Http/Controllers/ProductManagementController.php
+++ backend/app/Http/Controllers/ProductManagementController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\InventoryLog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Product;
@@ -44,6 +45,14 @@ class ProductManagementController extends Controller
 
         $product = Product::create($validated);
 
+        // Log the restore
+        InventoryLog::create([
+            'product_id' => $product->id,
+            'change_type' => 'restore',
+            'quantity_change' => $request->stock_quantity,
+            'reason' => 'Product Initial stock',
+        ]);
+
         return response()->json([
             'success' => true,
             'data' => $product
@@ -75,8 +84,59 @@ class ProductManagementController extends Controller
         if(!$item) return response()->json(['message' => 'product not found'], 404);
 
         $item->update(array_merge(
-            $request->only(['name', 'desc', 'description', 'price', 'stock_quantity'])));
+            $request->only(['name', 'desc', 'description', 'price'])));
         return response()->json($item, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * Update specific Product
+     */
+    public function updateStock(Request $request)
+    {
+
+        $validated = $request->validate([
+            'product_id' => 'required|integer',
+            'quantity' => 'required|integer',
+        ]);
+
+        $product = Product::findOrFail($request->product_id);
+        $oldStock = $product->stock_quantity;
+
+        // Update stock
+        $product->stock_quantity += $request->quantity;
+        $product->save();
+
+        // Determine change type and quantity difference
+        $difference = $product->stock_quantity - $oldStock;
+
+        if ($difference > 0) {
+            $changeType = 'addition';
+        } elseif ($difference < 0) {
+            $changeType = 'deduction';
+        } else {
+            $changeType = 'no_change';
+        }
+
+        // Only log if stock actually changed
+        if ($difference !== 0) {
+            InventoryLog::create([
+                'product_id' => $product->id,
+                'change_type' => $changeType,
+                'quantity_change' => $difference,
+                'reason' => 'Manual '.$changeType . ' of stock',
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Stock updated successfully.',
+            'data' => [
+                'product' => $product,
+                'change_type' => $changeType,
+                'quantity_change' => $difference,
+            ],
+        ]);
     }
 
     /**
